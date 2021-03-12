9fa4625586b27d4a29a185001e06c16f6d4add27
Yui <ytkakentan@yahoo.co.jp>
2021-03-12 19:43:07 +0900

    import css

--- function.php
+++ function.php
@@ -2,5 +2,5 @@
     function my_script_init() {
         wp_enqueue_style( 'style', get_stylesheet_uri() );
     }
-    add_action( 'wp_enqueue_scripts', 'my_script_init' );
+    add_action( 'wp_enqueue_style', 'my_script_init' );
 ?>
