86a93c3ce77553cafd6ae6d4afb7f5cd9edc2a0a
Yui <ytkakentan@yahoo.co.jp>
2021-03-17 07:12:03 +0900

    add menuber in header.php

--- functions.php
+++ functions.php
@@ -1,3 +1,17 @@
 <?php
+    // css読み込み
     wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+    // menuの項目作成
+    function register_my_menus() {
+        register_nav_menus(
+          array(
+            'about' => __( 'About' ),
+            'works' => __( 'Works' ),
+            'blog' => __( 'Blog' ),
+            'contact' => __( 'Contact' )
+           )
+         );
+       }
+       add_action( 'init', 'register_my_menus' );
 ?>
--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
     <script src="https://kit.fontawesome.com/779dc48fa9.js" crossorigin="anonymous"></script>
     <title><?php bloginfo('name'); ?></title>
   </head>
@@ -16,12 +15,20 @@
             <h1><?php bloginfo('name'); ?></h1>
         </div>
         <div class="row menu-container column-4 column-md-4 column-sm-2">
-            <nuv>
-                <?php wp_nav_menu(); ?>
+            <nav>
+                <?php wp_nav_menu(
+                    array(
+                        'theme_location' => 'about',
+                        'theme_location' => 'works',
+                        'theme_location' => 'blog',
+                        'theme_location' => 'contact'
+                        )
+                    );
+                ?>
                 <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-            </nuv>
+            </nav>
         </div>
         <div class="row sns column-1 column-sm-3 column-md-2">
             <i class="fab fa-instagram"></i>
