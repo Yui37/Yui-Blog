49deb44aebb7643259acfcc604867e56ed2d97ea
Yui <ytkakentan@yahoo.co.jp>
2021-03-19 01:54:40 +0900

    fix menue and add sns.php

--- header.php
+++ header.php
@@ -11,26 +11,25 @@
     <!--header start-->
     <header>
         <div class="row header-container full-width-container">
-        <div class="site-title column-7 column-sm-7 column-md-6">
-            <h1><?php bloginfo('name'); ?></h1>
-        </div>
-        <div class="row menu-container column-4 column-md-4 column-sm-2">
-            <nav>
-                <?php wp_nav_menu(
-                    array(
-                        'menu' => 'primary',
-                        )
-                    );
-                ?>
-                <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-            </nav>
-        </div>
-        <div class="row sns column-1 column-sm-3 column-md-2">
-            <i class="fab fa-instagram"></i>
-            <i class="fab fa-twitter"></i>
-        </div>
+            <div class="site-title column-7 column-sm-7 column-md-6">
+                <h1><?php bloginfo('name'); ?></h1>
+            </div>
+            <div class="row menu-container column-4 column-md-4 column-sm-2">
+                <nav>
+                    <?php wp_nav_menu(
+                        array(
+                            'menu' => 'primary',
+                            )
+                        );
+                    ?>
+                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                </nav>
+            </div>
+            <div class="row sns column-1 column-sm-3 column-md-2">
+                <?php get_template_part( 'sns' ); ?>
+            </div>
         </div>
     </header>
 <?php wp_head(); ?>
